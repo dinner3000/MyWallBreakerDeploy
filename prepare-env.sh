27806a3f66846af2bfa6c84cd0e981fd9a0f7bde
wallbreaker2017 <wallbreaker2017@yandex.com>
2017-02-03 12:46:40 +0000

    Update env script

--- prepare-env.sh
+++ prepare-env.sh
@@ -5,4 +5,6 @@ gcc autoconf libtool automake make \
 zlib-devel openssl-devel asciidoc xmlto \
 epel-release gettext pcre-devel asciidoc \
 xmlto libsodium-devel udns-devel libev-devel \
+mbedtls-devel libsodium-devel \
+libudns-dev udns-devel \
 git cvs -y
